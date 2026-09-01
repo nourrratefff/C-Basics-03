2a195d3da8b80319b76c6b8caf755ba23eabcc2c
Nour Atef <Nour Atef@LAPTOP-2M74GP4J>
2026-09-01 15:53:16 +0300

    Add project files.

---
+++ C#Basics-03.Answers/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace C_Basics_03.Answers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+            //Question 1
+
+
+            try
+            {
+                int divisor = 0;
+                int result = 10 / divisor;
+                
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"Error occured, Cannot proceed. {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Successfully calculated."
+            }
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+       
+    }
+}
